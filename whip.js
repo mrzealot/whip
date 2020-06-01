0e17922523e5d422147e39718034162733fd66d3
Bán Dénes <mr@zealot.hu>
2020-06-01 17:47:38 +0200

    Refactor + stats

--- whip.js
+++ whip.js
@@ -1,72 +1,20 @@
 #!/usr/bin/env node
 
-const fs = require('fs')
+//#region Includes
+
+const fs = require('fs-extra')
 const os = require('os')
 const path = require('path')
 const open = require('open')
 const yaml = require('js-yaml')
-const args = require('yargs').argv
 const moment = require('moment')
-const mkdirp = require('mkdirp')
 const readline = require('readline')
 
-let verbose = () => {}
+//#endregion
 
-const usage = (msg = '', code = 1) => {
-    if (msg) {
-        console.log(msg)
-        console.log()
-    }
-    console.log(`Common arguments:`)
-    console.log(`  -c/--config: specify the config file to use`)
-    console.log(`  -i/--input: specify/override the input file`)
-    console.log()
-    console.log(`Commands:`)
-    console.log(`coin (c)onfig -- list all current config key/value pairs`)
-    console.log(`coin (c)onfig key -- print config for a specific key`)
-    console.log(`coin (c)onfig key value -- set key to value in the config`)
-    console.log()
-    console.log(`coin ()(g)enerate -- generate the daily TODO list`)
-    console.log(`  --date -- override the date (default: today)`)
-    console.log(`  --format -- the path format to the daily logs, see moment.js`)
-    console.log(`  --noopen -- don't automatically open in editor`)   
-    // TODO console.log(`  --noyesterday -- don't look back to see what's overdue`)    
-    process.exit(code)
-}
+//#region Helpers
 
-
-
-
-
-
-// TODO look back to yesterday's file to see if anything's overdue
-const parse = async (file) => {
-    let raw = ''
-    let in_yaml = false
-
-    const rl = readline.createInterface({
-        input: fs.createReadStream(file)
-    })
-    
-    for await (const line of rl) {
-        if (line.trim() == '---') {
-            in_yaml = !in_yaml
-            if (!in_yaml) {
-                return yaml.safeLoad(raw)
-            }
-            continue
-        }
-
-        if (in_yaml) {
-            raw += line + '\n'
-        }
-    }
-}
-
-
-
-const actual = (when, now_override) => {
-    verbose(`Checking if ${when} is actual...`)
+const actual = (when, now) => {
     let [frequency, expander, restrictor] = when.split(/\s+/g)
 
     if (!expander || !expander.includes('/')) {
@@ -91,9 +39,6 @@ const actual = (when, now_override) => {
         restrictor = []
     }
     
-    verbose(`frequency=${frequency}, expander=${num}/${denom}, restrictor=${restrictor}`)
-
-    const now = moment(now_override)
     const weekEpoch = moment('1970-01-05 00:00:00')
     const monthEpoch = moment('1970-01-01 00:00:00')
     let epoch
@@ -161,134 +106,223 @@ const actual = (when, now_override) => {
     return true
 }
 
+const parse = async (file) => {
+    let raw = ''
+    let in_yaml = false
 
-
-
-class Commander {
-
-    constructor(config, args, lookup) {
-        this.aliases = {
-            u: 'usage',
-            c: 'config',
-            g: 'generate'
-        }
-        this.usage = usage
-        const valids = new Set(Object.values(this.aliases))
-
-        this.command = this.aliases[args._[0]] || args._[0]
-        if (!this.command) this.command = 'generate'
-        if (!valids.has(this.command)) usage(`Unknown command "${command}"`)
-
-        this.config_data = config
-        this.args = args
-        this.lookup = lookup
-    }
-
-    needs_input() {
-        return !['usage', 'config'].includes(this.command)
-    }
-
-    load(input_file, data) {
-        this.input_file = input_file
-        this.data = data
-    }
-
-    config() {
-        const conf = this.config_data
-        const [,key,val] = this.args._
-        if (key) {
-            if (val) {
-                conf[key] = val
-                fs.writeFileSync(this.args.config, yaml.safeDump(conf))
-                verbose(`Successfully set config key "${key}" to value "${val}"`)
-            } else {
-                console.log(conf[key])
-            }
-        } else {
-            console.log(conf)
-        }
-    }
-
-    generate() {
-        const format = this.lookup('format')
-        const now_override = this.lookup('date')
-        if (!format) usage('Missing format...')
-
-        let header = {}
-        for (const group of this.data) {
-            for (const task of group.subs) {
-                if (actual(task.when, now_override)) {
-                    header[group.name] = header[group.name] || {}
-                    header[group.name][task.name] = ''
+    const rl = readline.createInterface({
+        input: fs.createReadStream(file)
+    })
+    
+    for await (const line of rl) {
+        if (line.trim() == '---') {
+            in_yaml = !in_yaml
+            if (!in_yaml) {
+                try {
+                    return yaml.safeLoad(raw)
+                } catch (ex) {
+                    throw new Error(`Failed to parse ${file}`, ex)
                 }
             }
+            continue
         }
 
-        const now = moment(now_override)
-        const content = `---\n${yaml.safeDump(header)}---\n\n# ${now.format('dddd')} Notes:\n\n- `
-        const file = now.format(format)
-        if (fs.existsSync(file) && !this.lookup('force')) {
-            console.log(`File ${file} already exists, so you have to --force this!`)
-        } else {
-            mkdirp.sync(path.dirname(file))
-            fs.writeFileSync(file, content)
+        if (in_yaml) {
+            raw += line + '\n'
         }
-
-        if (!this.lookup('noopen')) open(file)
-    }
-
-    execute() {
-        return this[this.command]()
     }
 }
 
+const input = (file) => {
+    if (!file) throw new Error('Missing input parameter...')
+    if (!fs.existsSync(file)) throw new Error('Non-existent input file')
+    return yaml.safeLoad(fs.readFileSync(file, 'utf8'))
+}
 
+//#endregion
 
+//#region Commands
 
-;(async () => {
+let config = {} 
 
-    // logging (if necessary)
-    if (args.v || args.verbose) {
-        verbose = console.log.bind(console)
+require('yargs')
+.option('config', {
+    alias: 'c',
+    default: path.join(os.homedir(), '.whip_config'),
+    describe: 'Config yaml file',
+    type: 'string'
+})
+.config('config', (path) => {
+    if (!fs.existsSync(path)) {
+        console.log(`Initializing config file at ${path}`)
+        fs.mkdirpSync(path.dirname(path))
+        fs.writeFileSync(path, yaml.safeDump({}))
     }
-
-    // set up config
-    args.config = args.config || args.c
-    if (!args.config) {
-        args.config = path.join(os.homedir(), '.whip_config')
-    }
-    if (!fs.existsSync(args.config)) {
-        verbose(`Initializing config file at ${args.config}`)
-        mkdirp.sync(path.dirname(args.config))
-        fs.writeFileSync(args.config, yaml.safeDump({}))
-    }
-    verbose(`Loading config from ${args.config}`)
-    const config = yaml.safeLoad(fs.readFileSync(args.config, 'utf8'))
-
-    const lookup = ((config, args) => (key) => {
-        if (!Array.isArray(key)) {
-            key = [key]
+    return config = yaml.safeLoad(fs.readFileSync(path, 'utf8'))
+})
+.option('input', {
+    alias: 'i',
+    describe: 'Input schedule file',
+    type: 'string'
+})
+.option('verbose', {
+    alias: 'v',
+    default: false,
+    describe: 'Verbose log output',
+    type: 'boolean'
+})
+.command(['config [key] [value]', 'c'], 'Get or set config parameters', {}, (args) => {
+    if (args.key) {
+        if (args.value !== undefined) {
+            if (args.value == 'true' || args.value == 'yes') args.value = true
+            if (args.value == 'false' || args.value == 'no') args.value = false
+            config[args.key] = args.value
+            if (args.value == 'undefined') delete config[args.key]
+            fs.writeFileSync(args.config, yaml.safeDump(config))
+            console.log(`Successfully set config key "${args.key}" to value "${args.value}"`)
+        } else {
+            console.log(config[args.key])
         }
-        for (const k of key) {
-            if (args[k]) return args[k]
-            if (config[k]) return config[k]
-        }
-        return undefined
-    })(config, args)
+    } else {
+        console.log(config)
+    }
+})
+.command(['generate', 'g', '*'], 'Generate a daily TODO list', (yargs) => {
+    return yargs
+    .option('date', {
+        alias: 'd',
+        describe: 'Override date (default is today)',
+        type: 'string'
+    })
+    .option('format', {
+        alias: 'f',
+        default: '',
+        describe: 'Path format to daily log file',
+        type: 'string'
+    })
+    .option('yesterday', {
+        default: true,
+        describe: 'Whether to look back to yesterday\'s file to see if anything\'s overdue',
+        type: 'boolean'
+    })
+    .option('force', {
+        default: false,
+        describe: 'Whether to force (re)generation of existing file',
+        type: 'boolean'
+    })
+    .option('open', {
+        default: true,
+        describe: 'Whether to open the newly generated file in the default editor',
+        type: 'boolean'
+    })
+}, async (args) => {
+    if (!args.input) throw new Error('Missing input...')
+    if (!args.format) throw new Error('Missing format...')
+    if (!args.date) args.date = undefined
+    const now = moment(args.date)
 
-    // setting up the commander
-    const commander = new Commander(config, args, lookup)
-    if (commander.needs_input()) {
-        // parse input
-        const input = lookup(['input', 'i'])
-        if (!input) usage('Missing input...')
-        verbose(`Parsing input ${input}`)
-        const data = yaml.safeLoad(fs.readFileSync(input, 'utf8'))
-        commander.load(input, data)
+    const input_data = input(args.input)
+    let header = {}
+    for (const group of input_data) {
+        for (const task of group.subs) {
+            if (actual(task.when, now)) {
+                header[group.name] = header[group.name] || {}
+                header[group.name][task.name] = ''
+            }
+        }
     }
 
-    // execute command
-    verbose(`Executing command ${commander.command}`)
-    commander.execute()
+    if (args.yesterday) {
+        const yesterday = moment(now).subtract(1, 'day')
+        const old_log = yesterday.format(args.format)
+        const old_data = await parse(old_log)
+        for (const [group_key, group] of Object.entries(old_data)) {
+            for (const [key, val] of Object.entries(group)) {
+                if (!val || val == 'no') { // 'no' check added for YAML 1.1 compat
+                    header[group_key] = header[group_key] || {}
+                    header[group_key][key] = ''
+                }
+            }
+        }
+    }
 
-})()
+    const content = `---\n${yaml.safeDump(header)}---\n\n# ${now.format('dddd')} Notes:\n\n- `
+    const file = now.format(args.format)
+    if (fs.existsSync(file) && !args.force) {
+        console.log(`File ${file} already exists, so you have to --force this!`)
+    } else {
+        fs.mkdirpSync(path.dirname(file))
+        fs.writeFileSync(file, content)
+    }
+
+    if (args.open) open(file)
+})
+.command(['stats', 'stat', 's'], 'Generate stats from past TODOs', (yargs) => {
+    return yargs
+    .option('from', {
+        alias: 'f',
+        describe: 'Stat start date (default = a month ago)',
+        type: 'string'
+    })
+    .option('to', {
+        alias: 't',
+        describe: 'Stat end date (default = today)',
+        type: 'string'
+    })
+    .option('output', {
+        alias: 'o',
+        default: 'stats.csv',
+        describe: 'Output CSV file',
+        type: 'string'
+    })
+    .option('open', {
+        default: true,
+        describe: 'Whether to open the newly generated stats in the default viewer',
+        type: 'boolean'
+    })
+}, async (args) => {
+    if (!args.format) throw new Error('Missing format...')
+
+    const from = moment(args.from)
+    if (!args.from) from.subtract(1, 'month')
+    const to = moment(args.to)
+
+    const input_data = input(args.input)
+    let labels = {}
+    for (const group of input_data) {
+        for (const task of group.subs) {
+            labels[group.name] = labels[group.name] || []
+            labels[group.name].push(task.name)
+        }
+    }
+
+    let result = [['date']]
+    for (const [group_key, group] of Object.entries(labels)) {
+        for (const key of group) {
+            result[0].push(`${group_key}-${key}`)
+        }
+    }
+
+    while (from.isBefore(to)) {
+        const file = from.format(args.format)
+        const data = await parse(file)
+        const row = [from.format('YYYY-MM-DD')]
+        for (const [group_key, group] of Object.entries(labels)) {
+            for (const key of group) {
+                let val = data[group_key] ? data[group_key][key] : ''
+                val = val ? `"${val}"` : val
+                row.push(val)
+            }
+        }
+        result.push(row)
+        from.add(1, 'day')
+    }
+
+    const result_text = result.map(row => row.join(',')).join('\n')
+    fs.writeFileSync(args.output, result_text)
+    if (args.open) open(args.output)
+})
+.demandCommand()
+.argv
+    
+//#endregion
